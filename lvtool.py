0ed38cfda357b05c5ddfcdcbf5813f67d46195bf
LukaLukaLukaLuka <bendover35hc54ghbr@gmail.com>
2025-05-15 15:47:13 +0200

    LvTool Search
    Added a simple search function for programs and gives some information about the program

--- lvtool.py
+++ lvtool.py
@@ -3,28 +3,41 @@ import os
 import typer
 
 app = typer.Typer()
-d = {
+installlinks = {
     "write": 'curl -H "Authorization: token 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" -o write.py "https://raw.githubusercontent.com/LukaLukaLukaLuka/lvtool/refs/heads/master/Write/write.py"',
     "convert": 'curl -H "Authorization: token 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" -o convert.py "https://raw.githubusercontent.com/LukaLukaLukaLuka/lvtool/refs/heads/master/Convert/convert.py"',
 }
+programinfo = {
+    "write": "A simple write program that can be used standalone, but is meant to be imported",
+    "convert": "A simple conversion program to convert most file types",
+}
 
 
-def searchi(program):
-    os.system(d.get(program))
+def searchinstall(program):
+    os.system(installlinks.get(program))
 
 
-def searchu(program):
+def searchuninstall(program):
     os.system(f"del {program}.py")
+
+
+def searchsearch(program):
+    typer.echo(programinfo.get(program))
 
 
 @app.command()
 def install(programid: str):
-    searchi(programid)
+    searchinstall(programid)
 
 
 @app.command()
 def uninstall(programid: str):
-    searchu(programid)
+    searchuninstall(programid)
+
+
+@app.command()
+def search(programid: str):
+    searchsearch(programid)
 
 
 if __name__ == "__main__":
